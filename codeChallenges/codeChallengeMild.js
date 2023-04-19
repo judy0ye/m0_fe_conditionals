002b3f99c7e5024220ee07be67041326c93afb51
Judy Ye <judyy.ye@gmail.com>
2023-04-19 15:23:33 -0700

    Initial commit

---
+++ codeChallenges/codeChallengeMild.js
@@ -0,0 +1,12 @@
+
+// % is the remainder operator
+// It returns the remainder of what is leftover
+// after you divide two numbers
+
+var num = 1
+
+if (num % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd")
+}
